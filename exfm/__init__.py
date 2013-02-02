57e4112ef9da1d7a32e23ff99806c4d64f934cc3
rstets <romko.stets@gmail.com>
2013-02-02 13:14:47 +0200

    More refactoring.

--- exfm/__init__.py
+++ exfm/__init__.py
@@ -3,6 +3,9 @@
 Ex.fm wrappers
 """
 
+import requests
+import json
+
 from core import Track
 
 class ExFmClient():
@@ -10,6 +13,33 @@ class ExFmClient():
         self.username = username
         self.password = password
 
+    def search_by_title(self, title):
+        try:
+            search_url = "http://ex.fm/api/v3/song/search/{term}"
+            response = requests.get(search_url.format(
+                term=title
+            ))
+            decoded = json.loads(response.text)
+            return decoded['songs']
+        except Exception as e:
+            print("error: ", e)
+            return []
+
+    def add_to_loved_tracks(self, id):
+        try:
+            if id:
+                love_url = "http://ex.fm/api/v3/song/{id}/love"
+                response = requests.post(love_url.format(id=id), data={
+                    'username': self.username,
+                    'password': self.password
+                })
+                decoded = json.loads(response.text)
+                if decoded['status_code'] != 200:
+                    print("code: ", decoded['status_code'], "text: ", decoded['status_text'])
+                else:
+                    print("love!")
+        except Exception as e:
+            print("error: ", e)
 
 class ExFmTrack(Track, ExFmClient):
     def __init__(self, track, **kwargs):
@@ -26,40 +56,15 @@ class ExFmTrack(Track, ExFmClient):
         """
         Search for track on exfm by 'artist - title'
         """
-        try:
-            import requests
-            search_url = "http://ex.fm/api/v3/song/search/{term}"
-            response = requests.get(search_url.format(
-                term=repr(self)
-            ))
-            import json
-            decoded = json.loads(response.text)
-            songs = decoded['songs']
-            if songs:
-                self.id = songs[0]['id']
-                print("search: ", self, ". found id: ", self.id)
-        except Exception as e:
-            print("error: ", e)
+        songs = self.search_by_title(title=repr(self))
+        if songs:
+            self.id = songs[0]['id']
+            print("search: ", self, ". found id: ", self.id)
         return self
 
     def add(self):
         """
         Add a track to "loved" on ex.fm
         """
-        try:
-            if self.id:
-                love_url = "http://ex.fm/api/v3/song/{id}/love"
-                import requests
-                response = requests.post(love_url.format(id=self.id), data={
-                    'username': App.config['exfm']['username'],
-                    'password': App.config['exfm']['password']
-                })
-                import json
-                decoded = json.loads(response.text)
-                if decoded['status_code'] != 200:
-                    print("code: ", decoded['status_code'], "text: ", decoded['status_text'])
-                else:
-                    print("love!")
-        except Exception as e:
-            print("error: ", e)
+        self.add_to_loved_tracks(id=self.id)
         return self
